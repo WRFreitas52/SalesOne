b8ea165e0faed8d7ec74ef88c3aa39ec823313e7
Wiliam Freitas <Wiliam.freitas@live.com>
2023-05-10 17:51:33 -0300

    Other entities and second migration

--- SalesWebMVC/Models/Department.cs
+++ SalesWebMVC/Models/Department.cs
@@ -9,5 +9,34 @@ namespace SalesWebMVC.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        //#Construtors
+
+        public Department()
+        {
+
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        //# Methods
+
+        public void AddSeller( Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSeles(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+   
+        }
+
     }
 }
